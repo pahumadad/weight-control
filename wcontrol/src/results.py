8d3160f15b3c328ba86d6348a278c6825fcfa6c2
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-12 17:16:44 -0300

    Add function to get the skeletal muscle result

--- wcontrol/src/results.py
+++ wcontrol/src/results.py
@@ -1,10 +1,11 @@
-from wcontrol.conf.config import BMI, BFP
+from wcontrol.conf.config import BMI, BFP, MUSCLE
 
 
 class results(object):
     def __init__(self, control, gender):
         self.bmi = self.get_bmi(control.bmi)
         self.fat = self.get_fat(control.fat, gender)
+        self.muscle = self.get_muscle(control.muscle, gender)
 
     def get_bmi(self, bmi):
         for limit, msg in BMI:
@@ -16,4 +17,11 @@ class results(object):
             if gender == 'Feminine' and fat <= limit_w:
                 return msg
             if gender == 'Masculine' and fat <= limit_m:
+                return msg
+
+    def get_muscle(self, muscle, gender):
+        for limit_w, limit_m, msg in MUSCLE:
+            if gender == 'Feminine' and muscle <= limit_w:
+                return msg
+            if gender == 'Masculine' and muscle <= limit_m:
                 return msg
