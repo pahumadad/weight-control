5e4b8451df2fde4588478d0b3d052636a412b843
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-12 17:05:21 -0300

    Implement the BFP result

--- wcontrol/src/results.py
+++ wcontrol/src/results.py
@@ -1,11 +1,19 @@
-from wcontrol.conf.config import BMI
+from wcontrol.conf.config import BMI, BFP
 
 
 class results(object):
-    def __init__(self, control):
+    def __init__(self, control, gender):
         self.bmi = self.get_bmi(control.bmi)
+        self.fat = self.get_fat(control.fat, gender)
 
     def get_bmi(self, bmi):
         for limit, msg in BMI:
             if bmi <= limit:
+                return msg
+
+    def get_fat(self, fat, gender):
+        for limit_w, limit_m, msg in BFP:
+            if gender == 'Feminine' and fat <= limit_w:
+                return msg
+            if gender == 'Masculine' and fat <= limit_m:
                 return msg
