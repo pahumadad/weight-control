e25ca3e27b3c438ea2175bf4f8a411d5d966a943
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-31 21:37:19 -0400

    Make indexable

--- wcontrol/src/results.py
+++ wcontrol/src/results.py
@@ -3,10 +3,13 @@ from wcontrol.conf.config import BMI, BFP, MUSCLE, VISCERAL
 
 class results(object):
     def __init__(self, control, gender):
+        self.weight = ''
         self.bmi = self.get_bmi(control.bmi)
         self.fat = self.get_fat(control.fat, gender)
         self.muscle = self.get_muscle(control.muscle, gender)
+        self.rmr = ''
         self.visceral = self.get_visceral(control.visceral)
+        self.bodyage = ''
 
     def get_bmi(self, bmi):
         for limit, msg in BMI:
@@ -31,3 +34,19 @@ class results(object):
         for limit, msg in VISCERAL:
             if visceral <= limit:
                 return msg
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.weight
+        if index == 1:
+            return self.bmi
+        if index == 2:
+            return self.fat
+        if index == 3:
+            return self.muscle
+        if index == 4:
+            return self.visceral
+        if index == 5:
+            return self.rmr
+        if index == 6:
+            return self.bodyage
